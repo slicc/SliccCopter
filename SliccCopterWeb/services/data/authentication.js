932cceb741ab0fc332c5ad74f3a97172d67d3d22
OpenShift guest <steven_licciardi@hotmail.com>
2014-06-18 08:02:46 -0400

    starting mongo additions

--- SliccCopterWeb/services/data/authentication.js
+++ SliccCopterWeb/services/data/authentication.js
@@ -1,7 +1,51 @@
-(function(data) {
+(function(authentication) {
 	
-	data.authenticateUser = function (next) {
-		next(null, [{abc: "Hello", bvc: "There"}]);
+	var mongodb = require("mongodb");
+	var mongoUrl = "mongodb://localhost:27017/theBorad";
+	var theDb = null;
+	
+	authentication.getDb = function (next) {
+        if (!theDb) {
+          //connect to db
+          mongodb.MongoClient.connect(mongoUrl, function (err, db){
+              if (err) {
+                  next (err, null);
+              } else {
+                  theDb = {
+                      db: db,
+                      users: db.collection("users")
+                  };
+                  next(null, theDb);
+              }
+          });
+        }else {
+            next(null, theDb);
+        }
+	};
+	
+	authentication.authenticateUser = function (next) {
+        authentication.getDb(function (err, db) {
+           if (err) {
+               console.log("Fail to get database: " + err);
+               next(err, null);
+           } else {
+               //authenticate user
+               db.users.count(function(err, count){
+                   if (err) {
+                        next(err, null);
+                        console.log("Failed to retrieve users count: " + err);
+                   }else {
+                        db.users.find( { userName: "bob" } ).toArray(function (err, results) {
+                           if (err) {
+                                console.log("Failed to retrieve users: " + err);
+                           }  else {
+                                next(null, results);
+                           }
+                        });
+                   }
+               });
+           }
+        });
 	};
 
 })(module.exports);
